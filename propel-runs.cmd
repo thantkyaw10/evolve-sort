09bd9001c03daba634768a5ea58c4daa5a4811cc
thantkyaw10 <46037388+thantkyaw10@users.noreply.github.com>
2020-05-12 19:16:23 +0630

    Update propel-runs.cmd

--- propel-runs.cmd
+++ propel-runs.cmd
@@ -2,18 +2,18 @@
 universe    = vanilla
 notification = error
 notify_user  = h5thset@amherst.edu
-initialdir  = /mnt/scratch/h5thset/propel
+initialdir  = /mnt/scratch/h5thset/evolve-sort6/evolve-sort
 getenv = True
 executable  = run
 
 ## Job properties
-output = /home/h5thset/cluster-scratch/propel/results/0/out
-error  = /home/h5thset/cluster-scratch/propel/results/0/err
-log    = /home/h5thset/cluster-scratch/propel/results/0/log
+output = /home/h5thset/cluster-scratch/evolve-sort6/evolve-sort/results/0/out
+error  = /home/h5thset/cluster-scratch/evolve-sort6/evolve-sort/results/0/err
+log    = /home/h5thset/cluster-scratch/evolve-sort6/evolve-sort/results/0/log
 queue
 
 ## Job properties
-output = /home/h5thset/cluster-scratch/propel/results/1/out
-error  = /home/h5thset/cluster-scratch/propel/results/1/err
-log    = /home/h5thset/cluster-scratch/propel/results/1/log
+output = /home/h5thset/cluster-scratch/evolve-sort6/evolve-sort/results/1/out
+error  = /home/h5thset/cluster-scratch/evolve-sort6/evolve-sort/results/1/err
+log    = /home/h5thset/cluster-scratch/evolve-sort6/evolve-sort/results/1/log
 queue
